3f0e309e686b35368af9eb58c6775307e99338a8
UgrasK <ugurkaraer1989@gmail.com>
2023-08-22 01:22:33 +0300

    Add project files.

---
+++ MethodOverloadingDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloadingDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 9764;
+
+            Methods method = new Methods();
+            method.PrintConsole(num);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Methods
+    {
+        public void PrintConsole(string data)
+        {
+            Console.WriteLine(data);
+        }
+
+        public void PrintConsole(int data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
